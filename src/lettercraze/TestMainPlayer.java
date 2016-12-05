82043dc80ad51ea49e3101b0f658e142bde9dd62
Matthew Lamare <mmlamare@gmail.com>
2016-12-05 15:51:43 +0000

    TestMainPlayer.java created online with Bitbucket in Matt's branch

---
+++ src/lettercraze/TestMainPlayer.java
@@ -0,0 +1,13 @@
+import junit.framework.*;
+
+public class TestMainPlayer extends TestCase {
+   
+   void setUp(){
+   }
+
+   // test method to add two values
+   public void testMainPlayer(){
+      MainPlayer main = new MainPlayer();
+      main.main();
+   }
+}
