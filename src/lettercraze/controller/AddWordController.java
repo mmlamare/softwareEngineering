b1f3388ab0b1cc287769f20f72fc4bf1b11d0d7f
jtfakult <jtfakult@wpi.edu>
2016-12-05 13:33:11 -0500

    Added javadoc comments

--- src/lettercraze/controller/AddWordController.java
+++ src/lettercraze/controller/AddWordController.java
@@ -1,6 +1,5 @@
 package lettercraze.controller;
 
-import java.awt.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,11 +8,21 @@ import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 
+/**
+ * This ActionListener is used to prompt the user to add a word in the window builder.
+ * @see BuilderView
+ */
 public class AddWordController implements ActionListener
 {
 	JList list;
 	JFrame frame;
 	
+	/**
+	 * Initializer requires the frame and the JList that the new word will be added to
+	 * 
+	 * @param frame
+	 * @param l
+	 */
 	public AddWordController(JFrame frame, JList l)
 	{
 		this.frame = frame;
@@ -28,5 +37,4 @@ public class AddWordController implements ActionListener
 		DefaultListModel listModel = (DefaultListModel) list.getModel();
 		listModel.add(listModel.getSize(), word);
 	}
-
 }
