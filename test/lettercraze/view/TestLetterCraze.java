3de07493e693b1bc092399bc32072fc0ef64543e
mmlamare <mmlamare@gmail.com>
2016-12-08 13:40:29 -0500

    Added this test case

---
+++ test/lettercraze/view/TestLetterCraze.java
@@ -0,0 +1,12 @@
+package lettercraze.view;
+
+import junit.framework.TestCase;
+import lettercraze.model.Model;
+
+public class TestLetterCraze extends TestCase {
+
+	public void testLetterCraze(){
+		
+	}
+	
+}
