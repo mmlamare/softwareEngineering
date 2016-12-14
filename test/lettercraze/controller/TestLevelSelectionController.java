f0541abd622d8b672be6026656494ae94b98d4e4
ALewis <ALewis@DESKTOP-522TE4S>
2016-12-13 22:16:44 -0500

    Added level selection, lightning clock, and quit level controller tests

---
+++ test/lettercraze/controller/TestLevelSelectionController.java
@@ -0,0 +1,67 @@
+package lettercraze.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+
+import junit.framework.TestCase;
+import lettercraze.files.Data;
+import lettercraze.files.Resources;
+import lettercraze.model.Model;
+import lettercraze.view.LetterCraze;
+import lettercraze.view.MenuView;
+
+public class TestLevelSelectionController extends TestCase {
+	
+	Model m;
+	LetterCraze app;
+	MenuView mView;
+	
+	@Override
+	protected void setUp() {
+		Resources.loadResources();
+		// Ensure that we have access to the game's data directory
+		if (!Data.dataPathExists()) {
+			Data.createDataPath();
+		}
+		
+		m = new Model(Data.readScores());
+		app = new  LetterCraze(m);
+		mView = new MenuView(m, app);
+				
+	}
+	
+	@Override
+	protected void tearDown() {
+		app.dispose();
+	}
+		
+	public void testLockedLevel() {
+		setUp();
+		LevelSelectionController lscon = new LevelSelectionController(m,app);
+		assertFalse(m.isUnlocked(mView.getButton(8).getLevelID()));
+		ActionEvent ae = new ActionEvent(mView.getButton(8), java.awt.event.ActionEvent.ACTION_PERFORMED, "HOWDY PARTNER");
+		assertFalse(m.isInGame());
+		tearDown();
+	}
+	
+	public void testUnlockedLevel(){
+		setUp();
+		LevelSelectionController lscon = new LevelSelectionController(m,app);
+		assertTrue(m.isUnlocked(mView.getButton(0).getLevelID()));
+		ActionEvent ae = new ActionEvent(mView.getButton(0), java.awt.event.ActionEvent.ACTION_PERFORMED, "HOWDY PARTNER");
+		lscon.actionPerformed(ae);
+		assertTrue(m.isInGame());
+		assertEquals(0,m.currentGame.getLevelID());
+	}
+	
+	public void testBadLevelSource(){
+		setUp();
+		LevelSelectionController lscon = new LevelSelectionController(m,app);
+		assertTrue(m.isUnlocked(mView.getButton(0).getLevelID()));
+		ActionEvent ae = new ActionEvent(mView.getButton(14), java.awt.event.ActionEvent.ACTION_PERFORMED, "HOWDY PARTNER");
+		lscon.actionPerformed(ae);
+		assertFalse(m.isInGame());	
+	}
+
+	
+}
---
+++ test/lettercraze/controller/TestQuitLevelController.java
@@ -0,0 +1,48 @@
+package lettercraze.controller;
+
+import java.awt.event.ActionEvent;
+
+import junit.framework.TestCase;
+import lettercraze.files.Data;
+import lettercraze.files.Resources;
+import lettercraze.model.Level;
+import lettercraze.model.Model;
+import lettercraze.view.GameView;
+import lettercraze.view.LetterCraze;
+
+public class TestQuitLevelController extends TestCase {
+	Model m;
+	LetterCraze app;
+	GameView gView;
+	
+	@Override
+	protected void setUp() {
+		Resources.loadResources();
+		// Ensure that we have access to the game's data directory
+		if (!Data.dataPathExists()) {
+			Data.createDataPath();
+		}
+		
+		m = new Model(Data.readScores());
+		app = new  LetterCraze(m);
+		m.loadLevelByID(0);
+		gView = new GameView(m);				
+	}
+	
+	public void testQuitGame(){
+		setUp();
+		QuitLevelController qLC = new QuitLevelController(m,app);
+		assertTrue(m.isInGame());
+		ActionEvent ae = new ActionEvent(gView.quitButton, java.awt.event.ActionEvent.ACTION_PERFORMED, "HOWDY PARTNER");
+		for(int i=0; i<100;i++){
+			m.currentGame.scoreWord("test");
+		}
+		System.out.print("Here's our score hehexd");
+		System.out.println(m.currentGame.getScore());
+		
+		qLC.actionPerformed(ae);
+		assertFalse(m.isInGame());
+		
+	}
+}
+
