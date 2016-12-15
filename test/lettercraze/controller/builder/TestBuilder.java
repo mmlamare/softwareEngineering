342507f4b6d76629d1595eadad7468b4eaf336e0
jtfakult <jtfakult@wpi.edu>
2016-12-15 12:13:20 -0500

    Starting test cases for builder

---
+++ test/lettercraze/controller/builder/TestBuilder.java
@@ -0,0 +1,19 @@
+package lettercraze.controller.builder;
+
+import junit.framework.TestCase;
+
+public class TestBuilder extends TestCase
+{
+	
+	
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception
+	{
+		super.tearDown();
+	}
+
+}
