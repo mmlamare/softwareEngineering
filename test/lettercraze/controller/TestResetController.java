1d91c071e737e7a9e8738d700352a5b3946428d7
Cedar366 <Cedar366@DESKTOP-DD2019L>
2016-12-15 22:05:51 -0500

    Added a ResetController test for the player

---
+++ test/lettercraze/controller/TestResetController.java
@@ -0,0 +1,103 @@
+package lettercraze.controller;
+
+import java.awt.event.ActionEvent;
+
+import junit.framework.TestCase;
+import lettercraze.files.Data;
+import lettercraze.files.Resources;
+import lettercraze.model.Model;
+import lettercraze.model.board.Board;
+import lettercraze.model.game.PuzzleGame;
+import lettercraze.view.GameView;
+import lettercraze.view.LetterCraze;
+
+public class TestResetController extends TestCase {
+	Model m;
+	LetterCraze app;
+	GameView gView;
+	
+	@Override
+	protected void setUp() {
+		Resources.loadResources();
+		// Ensure that we have access to the game's data directory
+		if (!Data.dataPathExists()) {
+			Data.createDataPath();
+		}
+		
+		m = new Model(Data.readScores());
+		app = new  LetterCraze(m);	
+	}
+	
+	@Override
+	protected void tearDown(){
+		app.dispose();
+	}
+	
+	//Tests the Puzzle level reset
+	public void testResetGameNonL(){
+		setUp();
+		ResetController rC = new ResetController(m,app);
+		
+		m.loadLevelByID(0);
+		assertTrue(m.isInGame());
+		
+		gView = new GameView(m);	
+		assertTrue(m.getCurrentGame().getInfoString().contains("Puzzle"));
+		
+		PuzzleGame game= (PuzzleGame) m.getCurrentGame();
+		
+		Board bInitial = m.currentGame.getBoard();
+		
+		m.currentGame.scoreWord("hello");
+		
+		System.out.println("Printing Score: " + m.currentGame.getScore());
+		
+		ActionEvent ae = new ActionEvent(gView.resetButton, java.awt.event.ActionEvent.ACTION_PERFORMED, "HOWDY PARTNER");
+	
+		
+		rC.actionPerformed(ae);
+		Board bAfterReset = m.currentGame.getBoard();
+		assertNotSame(bInitial, bAfterReset);
+		assertTrue(m.isInGame());
+
+		System.out.println("Printing Score after reset:" + game.getScore());
+		
+		tearDown();
+	}
+	
+	
+	
+	public void testResetGameLightning(){
+		setUp();
+		ResetController rC = new ResetController(m, app);
+	
+		
+		//Check if loaded lightning level		
+		m.loadLevelByID(1);
+		gView = new GameView(m);	
+		assertTrue(m.isInGame());
+		assertTrue(m.getCurrentGame().getInfoString().contains("120"));
+		
+		
+		Board bInit = m.currentGame.getBoard();
+		
+		//Reset Board
+		ActionEvent ae = new ActionEvent(gView.resetButton, java.awt.event.ActionEvent.ACTION_PERFORMED, "HOWDY PARTNER");
+		rC.actionPerformed(ae);
+		Board bFinal = m.getCurrentGame().getBoard();
+		
+		//Let clock count down 2 seconds
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//Check if it ends on 119 or 118 even after board resets
+		//118 or 117 is to account for run time delays
+		assertNotSame(bFinal, bInit	);
+		assertTrue(m.getCurrentGame().getInfoString().contains("118")||m.getCurrentGame().getInfoString().contains("119"));
+	}
+	
+}
